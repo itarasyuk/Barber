8bac48b2ebbee384bffb4d3372afd5af9e1e31df
Igor Tarasyuk <itarasyuk@radiantrfid.com>
2015-09-26 23:46:46 +0300

    Sources reorganization

--- Barber.WebApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Barber.WebApp.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Barber.WebApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Barber.WebApp.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
